3c5a9327ae7ce9a92bc1e1e19b770189e274f158
chucasr <haruka0402toku@gmail.com>
2024-05-10 19:16:54 +0900

    Update mainpage.py

--- mainpage.py
+++ mainpage.py
@@ -8,9 +8,7 @@ import datetime
 import collections.abc
 #hyper needs the four following aliases to be done manually.
 collections.Iterable = collections.abc.Iterable
-collections.Mapping = collections.abc.Mapping
-collections.MutableSet = collections.abc.MutableSet
-collections.MutableMapping = collections.abc.MutableMapping
+
 #Now import hyper
 import hyper
 
